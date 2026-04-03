1bd823526369d43e08ae42eb62df480550c28f6f
daksh-devcodes <dakshanjali123@gmail.com>
2026-04-03 14:03:02 +0530

    Add files via upload

---
+++ begi_5.js
@@ -0,0 +1,18 @@
+for (let i = 1; i <= 5; i++) {
+    console.log(i);
+}
+for (let i = 8; i < 12; i++) {
+    console.log(i);
+}
+for ( let i = 0; i < 5; i++) {
+    console.log(i);
+}
+// reverse
+for (let i = 5; i >= 1; i--) {
+    console.log(i);
+}
+for (let i = 1; i <= 10; i++) {
+    if (i % 2 === 0) {
+        console.log(i);
+    }
+}
