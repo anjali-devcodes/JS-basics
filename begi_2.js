2e372ce227d54121d29112cced702049415151f2
daksh-devcodes <dakshanjali123@gmail.com>
2026-04-03 13:58:41 +0530

    Add files via upload

---
+++ begi_2.js
@@ -0,0 +1,24 @@
+let name = "manisha";
+let age = "29";
+let height = "153";
+let weight = "70"
+console.log(name);
+console.log(age);
+console.log(height);
+console.log(weight);
+let score = 50
+console.log(score);
+let username = "sweety";
+let marks = 95;
+console.log(typeof username);
+console.log(typeof marks);
+let a = 30;
+let b = 44;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+let o = 44;
+let g = 55;
+let plus = o + g;
+console.log("result is:", plus);
