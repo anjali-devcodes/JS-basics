d48bfac7b5338c2844d80d7ec8e4ca47df2bbdac
daksh-devcodes <dakshanjali123@gmail.com>
2026-04-03 13:54:06 +0530

    Add files via upload

---
+++ beginner1.js
@@ -0,0 +1,29 @@
+let yourName = "Anjali Daksh";
+let yourAge = 18;
+let isStudent = "class-9"
+console.log(yourName);
+console.log(yourAge);
+console.log(isStudent);
+let a = 15;
+let b = 4;
+console.log(a + b);
+console.log(a * b);
+console.log(a - b);
+console.log(a % b);
+console.log(5 == "5");
+console.log(5 === "5");
+let digit = 36;
+console.log(digit == "36");
+console.log(digit === "36");
+let isLoggedin = true;
+let isAdmin = false;
+console.log(isLoggedin && false);
+console.log(isAdmin || true);
+console.log(!isAdmin);
+let marks = 75;
+if (marks >= 50) {
+    console.log("pass");
+}
+else {
+    console.log("fail");
+}
