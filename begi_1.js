57802d7989523d48641e61f2fda4195f9b141576
daksh-devcodes <dakshanjali123@gmail.com>
2026-04-03 13:57:57 +0530

    Add files via upload

---
+++ begi_1.js
@@ -0,0 +1,34 @@
+console.log("hello, i started java script");
+let name = "anjali";
+let age = "26";
+let height = "152cm"
+console.log(name);
+console.log(age);
+console.log(height);
+alert("hello anjali");
+let score = 10;
+score = 50;
+console.log(score);
+const pi = 3.14;
+console.log(pi);
+//data type
+let username = "sweety"
+let marks = 96;
+let ispassed = true;
+console.log(typeof username);
+console.log(typeof marks);
+console.log(typeof ispassed);
+let a = 10;
+let b = 5;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+console.log(5 == "5");
+console.log(5 === "5");
+let x = 20;
+let y = 10;
+let result = x + y;
+console.log("result is:", result)
+
+
