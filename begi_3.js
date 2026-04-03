d54a743a39fd8725087bd02a08d67b7a0ed5e920
daksh-devcodes <dakshanjali123@gmail.com>
2026-04-03 14:00:05 +0530

    Add files via upload

---
+++ begi_3.js
@@ -0,0 +1,43 @@
+// variable behaviour
+let name = "anjali";
+name = "manisha";
+console.log(name);
+
+const agee = 20;
+console.log(agee);
+
+let x = 19; // number
+console.log(typeof x);
+x = "hello"; // string
+console.log(typeof x);
+
+let a = true; //boolean
+let b = "JS"; //string
+let c = 100; //number
+console.log(typeof a);
+console.log(typeof b);
+console.log(typeof c);
+let num = 10;
+num += 5;
+num *= 2;
+console.log(num);
+let numm = 20;
+numm += 7;
+numm *= 5;
+numm -= 4;
+console.log(numm);
+let age = 26;
+console.log(age > 18 && age < 55);
+console.log(age > 5 && age < 7);
+console.log(!(age > 9));
+console.log(Boolean(0));
+console.log(Boolean(""));
+console.log(Boolean("hello"));
+console.log(Boolean(100));
+console.log(Number(20));
+let namee = "heinaaaa";
+console.log(namee.length);
+console.log(namee.toUpperCase());
+let nameee = "jabe";
+let ageee = 66;
+console.log(`My name is ${nameee} and I am ${ageee} years old`)
