978bfc72d72abfa92041c127f06254dbd1e02f42
daksh-devcodes <dakshanjali123@gmail.com>
2026-04-03 14:01:33 +0530

    Add files via upload

---
+++ begi_4.js
@@ -0,0 +1,76 @@
+let age = 40;
+if (age > 18) {
+    console.log("adult");
+}
+// learningd
+let womenage = 19;
+if (womenage > 18) {
+    console.log("available for this post");
+}
+else {
+    console.log("nnot available");
+}
+// learning else if
+let marks = 88;
+if (marks > 90) {
+    console.log("grade A");
+}
+else if (marks > 65) {
+    console.log("grade B");
+}
+else {
+    console.log("grade C")
+}
+// and condition
+let number = 43;
+if (number > 20 && number < 50) {
+    console.log("pass");
+}
+// or condition
+let day = "friday";
+if (day === "saturday" || day ==="friday") {
+    console.log("weekend");
+}
+// strict condition
+let title = "";
+if (title) {
+    console.log("accept");
+}
+else {
+    console.log("not accepted");
+}
+// learning if and else
+let num = "49";
+if (num === 49) {
+    console.log("equal");
+}
+else {
+    console.log("not equal");
+}
+// challenge 1
+let temperature = 25;
+if (temperature > 30) {
+    console.log("hot");
+}
+else {
+    console.log("normal");
+}
+// challenge 2
+let score = 33;
+if (score >= 33) {
+    console.log("pass");
+}
+else {
+    console.log("fail");
+}
+// challenge 3
+let level = 7;
+if (level < 3) {
+    console.log("looser")
+}
+else if (level < 6) {
+    console.log("average")
+}
+else {
+    console.log("gamer")
+}
