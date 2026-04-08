72a2ba5f950f2028ea634a02fe96aa7f06270f53
daksh-devcodes <dakshanjali123@gmail.com>
2026-04-08 15:40:36 +0530

    Add files via upload

---
+++ beginner2.js
@@ -0,0 +1,67 @@
+let a = 10;
+a = 20;
+console.log(a);
+// hoisting check
+console.log(x);
+var x = 10;
+// Type conversion
+let y = "52";
+console.log("52" + 2);
+console.log("52" - 2);
+let b = "100";
+console.log(Number(b));
+let m = "50";
+let n = 10;
+console.log(Number(m) + n);
+let g = 0;
+console.log(Boolean(g));
+let price = "200";
+let tax = "50";
+console.log(price + tax);
+let prices = "200";
+let taxes = "50";
+console.log(Number(prices) + Number(taxes));
+let vallue = "0";
+console.log(Boolean(vallue));
+let f = "123";
+let d = 77;
+console.log(Number(f) + d);
+let u = "hello";
+if ("hello") {
+    console.log("run");
+}
+let t = Number("hello");
+console.log(isNaN(t));
+// templates literals 
+let o = "sweety";
+let G = 67;
+console.log(`MY NAME IS ${o} and my age is ${G}`);
+let age = 20;
+age = 25;
+console.log(age);
+const ages = 20;
+console.log(age);
+let r = "10";
+let e = 5;
+console.log("10" + 5);
+console.log("10" - 5);
+let val = "50";
+console.log(Number("50"));
+console.log(Boolean("50"));
+let data = "";
+if (data) {
+    console.log("valid");
+}
+else {
+    console.log("invalid");
+}
+let yy = "sweety";
+let xx = 55;
+console.log(`my name is ${yy} and age is ${xx}`);
+let usernum = prompt("enter your number");
+if (usernum!=="" && !isNaN(usernum)) {
+    console.log("valid number");
+} else {
+    console.log("invalid number");
+}
+
